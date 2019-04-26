021e23d53350ca94084187845aa16171cd96c3e8
Sebastian <sebastian.kuhlgert@gmail.com>
2019-04-26 19:18:19 -0400

    Add globals

---
+++ R/globals.R
@@ -0,0 +1,8 @@
+# Global Variables
+
+photosynq.env <- new.env()
+photosynq.env$DEFAULT_API_DOMAIN <- "https://photosynq.org"
+photosynq.env$API_DOMAIN <- photosynq.env$DEFAULT_API_DOMAIN
+photosynq.env$API_PATH <- "api/v3"
+photosynq.env$EMAIL <- NULL
+photosynq.env$TOKEN <- NULL
