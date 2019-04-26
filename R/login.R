d7c941a7b855abaef5f87abad94a4ab2e1d6202a
Sebastian <sebastian.kuhlgert@gmail.com>
2019-04-26 19:19:42 -0400

    Update login

--- R/login.R
+++ R/login.R
@@ -1,15 +1,17 @@
 #' Login to PhotosynQ
 #'
-#' This function allows you to login to PhotosynQ and receive your authentication token.
-#' The functions getProjectInfo(), getProjectData() and logout() require the token to
-#' receive data from PhotosynQ
+#' This function allows you to login to PhotosynQ and start your session.
+#' The functions getProjectInfo(), getProjectData(), getProject() and logout()
+#' require a session started by login in.
+#' Login is only required once at the beginning of a session.
 #' @param email Your email address you use to login
+#' @param url (optional) Change the default URL to point to another instance
 #' @keywords login
-#' @export
+#' @export login
 #' @examples
 #' login("john.doe@domain.com")
 
-login <- function(email=""){
+login <- function(email = "", url = photosynq.env$DEFAULT_API_DOMAIN){
     if(email !=""){
         httrFound <- require("httr",quietly = TRUE, warn.conflicts = FALSE, character.only = TRUE)
         if(!httrFound){
@@ -24,27 +26,37 @@ login <- function(email=""){
         pwd <- getPass(msg = "Your PhotosynQ Password: ", forcemask = FALSE)
         if(is.null(pwd)){
             cat("Info: Login canceled.\n")
-            return(NULL) 
         }
-        url <- "https://photosynq.org/api/v3/sign_in.json"
+        if(url != photosynq.env$DEFAULT_API_DOMAIN){
+            assign( "API_DOMAIN", url, envir = photosynq.env )
+        }
+        else{
+            assign( "API_DOMAIN", photosynq.env$DEFAULT_API_DOMAIN, envir = photosynq.env )
+        }
+        url <- paste(url,photosynq.env$API_PATH, "sign_in.json", sep="/")
         request <- httr::POST(url, body= list("user[email]" = email,"user[password]" = pwd))
         if(status_code(request) == 500){
             cat("Warning: Failed to login.\n")
-            return(NULL)
         }
-        content <- content(request)
-        if(content$status == "success"){
-            cat("Successfully signed in as: ", content$user$name,"\n", sep=" ")
-            result <- list(email=content$user$email,token=content$user$auth_token,name=content$user$name)
-            return(result)
-        }
-        else if(content$status == "failed"){
-            cat(paste(content$notice,"\n", sep=""))
-            return(NULL)
+        else{
+            content <- content(request)
+            if(length(content) == 0){
+                cat("Warning: Failed to login.\n")
+            }
+            else{
+                if(content$status == "success"){
+                    cat("Successfully signed in as: ", content$user$name,"\n", sep=" ")
+                    result <- list(email=content$user$email,token=content$user$auth_token,name=content$user$name)
+                    assign( "EMAIL", content$user$email, envir = photosynq.env )
+                    assign( "TOKEN", content$user$auth_token, envir = photosynq.env )
+                }
+                else if(content$status == "failed"){
+                    cat(paste(content$notice,"\n", sep=""))
+                }
+            }
         }
     }
     else {
         cat("Warning: Please provide your email to login.\n")
-        return(NULL)
     }
 }
