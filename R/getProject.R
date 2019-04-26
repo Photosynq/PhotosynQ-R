96f44ae163c08a49c5d599724098f599983414f8
Sebastian <sebastian.kuhlgert@gmail.com>
2019-04-26 19:20:21 -0400

    Update Project

--- R/getProject.R
+++ R/getProject.R
@@ -1,38 +1,32 @@
 #' Get Project data in a Data Frame from PhotosynQ
 #'
 #' This function allows you to receive data from PhotosynQ and convert it into a  data frame.
-#' @param email Your email address you use to login
 #' @param projectID The ID of your Project (Just copy the Project ID from the project page or your user page)
-#' @param processedData Receive the processed data when set to TRUE, receive raw Data when set to FALSE. (raw data will increases data size significantly!)
-#' @param rawTraces Adds raw traces to processed data. It is ignored when processedData is set to TRUE. (increases data size significantly!)
+#' @param processedData (optional) Receive the processed data when set to TRUE, receive raw Data when set to FALSE. (raw data will increases data size significantly!)
+#' @param rawTraces (optional) Adds raw traces to processed data. It is ignored when processedData is set to TRUE. (increases data size significantly!)
 #' @keywords Project Data 
-#' @export
+#' @export getProject
 #' @examples
-#' getProject("john.doe@domain.com",1566,FALSE)
+#' getProject(1566)
 
-getProject <- function(email="", projectID="", processedData = TRUE, rawTraces = FALSE){
-    if(email !="" && projectID != ""){
-        login <- PhotosynQ::login(email)
+getProject <- function(projectID = "", processedData = TRUE, rawTraces = FALSE){
+    if(projectID != ""){
+        project_info <- PhotosynQ::getProjectInfo(projectID)
+        
+        if(!is.null(project_info)){
+            # Print Project name
+            cat(paste("Project:", project_info$name, "\n", sep=" "))
 
-        if(!is.null(login)){
-            project_info <- PhotosynQ::getProjectInfo(login$email, login$token, projectID)
-            
-            if(!is.null(project_info)){
-                # Print Project name
-                cat(paste("Project:", project_info$name, "\n", sep=" "))
+            project_data <- PhotosynQ::getProjectData(projectID, processedData, TRUE)
+            if(!is.null(project_data)){
 
-                project_data <- PhotosynQ::getProjectData(login$email, login$token, projectID, processedData, TRUE)
-                if(!is.null(project_data)){
-
-                    dl <- createDataframe(project_info,project_data)
-                    cat("Done\n")
-                    return(dl)
-                }
+                dl <- createDataframe(project_info,project_data)
+                cat("Done\n")
+                return(dl)
             }
         }
     }
-    else {
-        cat("You have to supply email, password and project ID\n")
-        return(NULL)
+    else{
+        cat("Warning: You have to provide a Project ID!\n")
     }
 }
